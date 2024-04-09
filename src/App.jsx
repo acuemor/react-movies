3b40e8db5a05fe8382a0498f13e0b7bc799dca35
Abel <abel.cuevas.contractor@bbva.com>
2024-04-09 21:12:42 +0200

    feat(*): remove unnecessary code

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,14 @@ import './App.css'
 import { mockedPodcasts } from './mocks.js';
 import SearchBar from './components/SearchBar.jsx';
 import ProductList from './components/ProductsList.jsx';
-import { filterItemsByName } from './utils/utils.jsx';
-
 
 export default function FilterableList() {
   const [query, setQuery] = useState('');
-
-  const filteredItems = filterItemsByName(mockedPodcasts, query)
 
   return (
     <>
       <SearchBar query={query} onType={(e) => setQuery(e.target.value)} />
       <ProductList products={mockedPodcasts} filterText={query} />
-      {/* <ProductList items={filteredItems} /> */}
     </>
   );
 }
--- src/utils/utils.jsx
+++
@@ -1,8 +0,0 @@
-export function filterItemsByName(items, query) {
-	query = query.toLowerCase();
-	return items.filter(item =>
-		item.collectionName.split(' ').some(word =>
-			word.toLowerCase().startsWith(query)
-		)
-	);
-}
